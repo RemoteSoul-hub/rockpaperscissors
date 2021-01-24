179d782535efe968e1db558dec6b5d46ce52c8ea
RemoteSoul-Hub <yahya.tahoune@gmail.com>
2021-01-25 00:04:01 +0100

    Add computer options to js, cleaner code

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ const game = ()=> {
     let pScore = 0; /* player score */
     let cScore = 0;  /* computer score */
 
+    //Starts the Game
+
     const startGame = () =>{
         const playBtn = document.querySelector('.intro button');
         const introScreen = document.querySelector('.intro');
@@ -13,8 +15,27 @@ const game = ()=> {
         });
     };
 
+    // Play Match
+const playMatch = () => {
+    const options = document.querySelectorAll('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const cpuHand = document.querySelector('.cpu-hand');
+    // Computer Options 
+    const computerOptions = ['rock', 'paper', 'scissors'];
+options.forEach(option => {
+    option.addEventListener('click', function() {
+        // Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+        
+    });
+});
+
+};
+
     // Call all the inner functions 
     startGame();
+    playMatch();
 };
 
 // start the game function 
